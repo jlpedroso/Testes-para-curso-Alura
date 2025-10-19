e670ad52317dfec9d573ed8235111fb10423775a
Jose Luis Pedroso <jlpedroso@gmail.com>
2025-10-18 21:16:25 -0300

    Incluir acesso ao pacote NuGet da DLL gerada no curso 19-34 e com ela gerar CSV e PDF com base em uma lista de dados.

---
+++ Teste para cursos/GeraRelatorio.cs
@@ -0,0 +1,35 @@
+﻿namespace TesteParaCursos;
+
+using ReportGenerator.Generators;
+
+internal class Curso1934 
+{
+
+    // CURSO 19/34 - DESENVOLVIMENTO E GERENCIAMENTO DE BIBLIOTECAS
+
+    internal void GeraRelatorios(List<Dictionary<string, string>> data)
+    {
+        CSVReportGenerator relatorio = new CSVReportGenerator(data);
+
+        relatorio.Title = "Relatório de pessoas";
+        relatorio.HeadLine = "Lista de pessoas cadastradas no sistema";
+        relatorio.FooterLine = "Total de pessoas: " + data.Count;
+
+        string arquivo = relatorio.GenerateReport();
+
+        Console.WriteLine($"O arquivo {arquivo} foi gerado corretamente");
+
+        PDFReportGenerator pdf = new PDFReportGenerator(data);
+
+        pdf.Title = "Relatório de pessoas";
+        pdf.HeadLine = "Lista de pessoas cadastradas no sistema";
+        pdf.FooterLine = "Total de pessoas: " + data.Count;
+
+        string arquivoPDF = pdf.GenerateReport();
+
+        Console.WriteLine($"O arquivo {arquivoPDF} foi gerado corretamente");
+
+    }
+
+}
+
--- Teste para cursos/Program.cs
+++ Teste para cursos/Program.cs
@@ -1,2 +1,32 @@
-﻿// Projeto para fazer testes nos cursos da Alura
+﻿using TesteParaCursos;
+
+// Projeto para fazer testes nos cursos da Alura
+
+List<Dictionary<string, string>> dados = new()
+{
+    new Dictionary<string, string>
+    {
+        {"Nome","Alice" },
+        {"Idade","38" },
+        {"Cidade" , "Salvador" }
+    },
+    new Dictionary<string, string>
+    {
+        {"Nome","Jose Luis" },
+        {"Idade","47" },
+        {"Cidade" , "Osasco" }
+    },
+    new Dictionary<string, string>
+    {
+        {"Nome","Priscila" },
+        {"Idade","45" },
+        {"Cidade" , "São Paulo" }
+    }
+};
+
+Curso1934 curso1934 = new Curso1934();
+
+curso1934.GeraRelatorios(dados);
+
+
 
